ff2487d47533971ccfd6c1d5fbab35463b38eb2d
fabianchoxD <fabianchoxd@gmail.com>
2018-03-31 01:29:15 -0500

    Second Commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCiyoenTmtDuxWMOrx1ejsY7ut-iz4aSgc",
+    authDomain: "angularfirebasecrud-289fc.firebaseapp.com",
+    databaseURL: "https://angularfirebasecrud-289fc.firebaseio.com",
+    projectId: "angularfirebasecrud-289fc",
+    storageBucket: "angularfirebasecrud-289fc.appspot.com",
+    messagingSenderId: "158063693234"
+  }
 };
