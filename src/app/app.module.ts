bb7ce5bb22c7fadf28fe0984cccb9dcc94890788
fabianchoxD <fabianchoxd@gmail.com>
2018-03-31 00:52:29 -0500

    First Commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,44 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { ToastrModule, Toast } from 'ngx-toastr';
+
+import { AppComponent } from './app.component';
+
+//Firebase
+import { AngularFireModule} from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { environment } from '../environments/environment';
+
+//components
+import { ProductsComponent } from './components/products/products.component';
+import { ProductListComponent } from './components/products/product-list/product-list.component';
+import { ProductComponent } from './components/products/product/product.component';
+
+//services
+import { ProductService } from './services/product.service'
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProductsComponent,
+    ProductListComponent,
+    ProductComponent
+  ],
+  imports: [
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot()
+  ],
+  providers: [
+    ProductService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/environments/environment.ts
@@ -0,0 +1,16 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCiyoenTmtDuxWMOrx1ejsY7ut-iz4aSgc",
+    authDomain: "angularfirebasecrud-289fc.firebaseapp.com",
+    databaseURL: "https://angularfirebasecrud-289fc.firebaseio.com",
+    projectId: "angularfirebasecrud-289fc",
+    storageBucket: "angularfirebasecrud-289fc.appspot.com",
+    messagingSenderId: "158063693234"
+  }
+};
